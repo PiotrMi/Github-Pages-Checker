8141f9032f782a891535e9e136640b57bf214983
Adam Wankowski <peter.wanko@gmail.com>
2023-03-09 11:15:48 +0100

    works not really

--- a.py
+++ a.py
@@ -1,12 +1,18 @@
 import requests
 
 # Replace 'username' with your GitHub username
-username = 'username'
+username = 'piotrmi'
 
 # Fetch all repositories owned by the user
 url = f'https://api.github.com/users/{username}/repos'
 response = requests.get(url)
 repos = response.json()
+
+print(response.text)
+
+if isinstance(repos, dict) and repos.get('message') == 'Not Found':
+    print(f'Error: {repos["message"]}')
+    exit()
 
 # Filter repositories that have GitHub Pages enabled
 pages_repos = []
@@ -14,8 +20,9 @@ for repo in repos:
     url = f'https://api.github.com/repos/{username}/{repo["name"]}/pages'
     response = requests.get(url)
     pages = response.json()
-    if pages['message'] != 'Not Found':
-        pages_repos.append(repo['name'])
+    if pages.get('message') == 'Not Found':
+        continue
+    pages_repos.append(repo['name'])
 
 # Print the list of repositories with GitHub Pages enabled
 print(f'Repositories with GitHub Pages enabled: {", ".join(pages_repos)}')
